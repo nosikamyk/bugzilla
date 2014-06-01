1c726a5e73b4462fde4e7fd1f348a03c88089532
Wojti <nosikamyk89@gmail.com>
2014-06-01 19:48:51 +0200

    account done

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,7 +1,6 @@
-class ConfirmationsController < Devise::ConfirmationsController
-  def after_confirmation_path_for(resource_name, resource)
-    if current_user.last_sign_in_at.nil? then
-      redirect_to :controller => :statics, :action => :home
-    end
+class ConfirmationsContr < Devise::confirmations_controller
+  private
+  def create
+    redirect_to controller: :statics, action: :home
   end
 end
